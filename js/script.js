7b1208b816d0e1d2a2c8f249637ea88d89dda6e6
Sam <samu3lf3lixx@gmail.com>
2026-07-06 17:38:18 -0300

    adicionado: easter egg, botao com estetica antiga

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
-const butao = document.querySelector('button')
-const ispraite = document.querySelector('.image')
-butao.addEventListener('click', ()=>{
-    ver()
-})
-async function ver() {
-    const resposta = await fetch(`https://dog.ceo/api/breeds/image/random`)
-    const data = await resposta.json()
-    const url = data.message
-    ispraite.src = url
-    ispraite.style.display = 'block'
-}
+const butao = document.querySelector("button");
+const ispraite = document.querySelector(".image");
+let conter = document.querySelector(".counter");
+let i = 0;
+butao.addEventListener("click", () => {
+  ver();
+  i += 1;
+  conter.innerHTML = i;
 
+  if (i >= 100) {
+    conter.className = "texto-rgb";
+  }
+});
+async function ver() {
+  try {
+    const resposta = await fetch("https://random.dog/woof.json");
+    const data = await resposta.json();
+    const url = data.url;
+    if (url.endsWith(".mp4") || url.endsWith(".webm")) {
+      ver();
+      return;
+    }
+    ispraite.src = url;
+    ispraite.style.display = "block";
+  } catch (err) {
+    console.log(err);
+  }
+}
